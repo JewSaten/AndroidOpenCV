bcf24fbd7ab67e504aac46599267b9afffa4e764
JewSaten <satenjew@gmail.com>
2017-10-12 18:37:43 +0800

    Initial commit

---
+++ app/src/main/java/io/jewsaten/opencv/MainActivity.java
@@ -0,0 +1,78 @@
+package io.jewsaten.opencv;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    static {
+        OpenCVLoader.initialize();//采用静态初始化，不依赖Manager.apk
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.btnGaussian);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        btn.setTag(imageView);
+    }
+
+    public void onGaussianBlur(View view) {
+        final ImageView target = (ImageView) view.getTag();
+        Bitmap src = ((BitmapDrawable) target.getDrawable()).getBitmap();
+
+        Observable.just(src).map(new Func1<Bitmap, Bitmap>() {
+            @Override
+            public Bitmap call(Bitmap bitmap) {
+                Mat mat = new Mat(bitmap.getHeight(), bitmap.getWidth(), CvType.CV_8UC4);
+                Utils.bitmapToMat(bitmap, mat);
+
+                Imgproc.GaussianBlur(mat, mat, new Size(29, 29), 0);
+
+                Bitmap dst = Bitmap.createBitmap(mat.cols(), mat.rows(), Bitmap.Config.ARGB_8888);
+                Utils.matToBitmap(mat, dst);
+
+                return dst;
+            }
+        }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(new Subscriber<Bitmap>() {
+            @Override
+            public void onCompleted() {
+                Log.d(TAG, "onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, e.getMessage());
+            }
+
+            @Override
+            public void onNext(Bitmap bitmap) {
+                target.setImageBitmap(bitmap);
+            }
+        });
+    }
+
+
+}
